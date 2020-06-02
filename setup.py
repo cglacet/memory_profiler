d2f4646daf234cfd282864e3839c28c77a744ec7
Christian glacet <christian.glacet@gmail.com>
2020-06-02 15:12:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ Operating System :: Unix
 """
 
 setup(
-    name='cglacet/memory_profiler',
+    name='cglacet-memory_profiler',
     description='A module for monitoring memory usage of a python program',
     long_description=open('README.rst').read(),
     version=find_version("memory_profiler.py"),
